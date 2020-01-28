ece655cc38e52ff2b34594d8e8e8ac880e5ca235
dongho108 <54317630+dongho108@users.noreply.github.com>
2020-01-28 22:22:37 +0900

    결과값은 나온다. 단 "붙어서"입력 해결 못했다.

--- main.cpp
+++ main.cpp
@@ -11,44 +11,106 @@
 using namespace std;
 
 int main() {
-    int r, c; //가로 i, 세로 j
+    int c, r;
     cin >> c >> r;
     
+    int tr = r+2; //실제 만들어지는 미로의 크기 (입력되는 미로 주위를 0으로 둘러쌈)
+    int tc = c+2;
+    
     //미로, 최단거리 기록 배열, 방문확인 배열
-    int** miro = new int*[r];
-    int** sp = new int*[r];
-    bool** visit = new bool*[r];
-    for(int count = 0 ; count < r; ++count){
-        miro[count] = new int[c];
-        sp[count] = new int[c];
-        visit[count] = new bool[c];
+    int** miro = new int*[tc];
+    int** sp = new int*[tc];
+    bool** visit = new bool*[tc];
+    for(int count = 0 ; count < tc; ++count){
+        miro[count] = new int[tr];
+        sp[count] = new int[tr];
+        visit[count] = new bool[tr];
     }
     
     //미로 민들기, visit배열 초기화, sp배열 초기화
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            cin >> miro[i][j];
+    //미로 주위를 0으로 채우야한다.
+    for(int i=0; i<tc; i++){
+        for(int j=0; j<tr; j++){
+            cout<<"miro["<<i<<"]["<<j<<"]"<<endl;
+            if(1<=i && i<=c && 1<=j && j<=r){
+                                
+                cin >> miro[i][j];
+            }
+                
+            else{
+                miro[i][j] = 0;
+                
+            }
+                
             visit[i][j] = false;
             sp[i][j] = 10000000; // 최대수
         }
     }
-    sp[0][0] = 1;
+    cout << "입력끝" << endl;
+    sp[1][1] = 1;
     //bfs하면서 최단거리 기록
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
+    //1,1 부터 시작
+    for(int i=1; i<tr; i++){
+        for(int j=1; j<tc; j++){
+            if(visit[i][j] == true)
+                continue;
+            cout<<"miro["<<i<<"]["<<j<<"]"<<endl;
             queue <int> x;
             queue <int> y;
             x.push(i);
-            x.push(j);
+            y.push(j);
             
             while(!x.empty()){
                 int xt = x.front();
                 int yt = y.front();
+                visit[xt][yt] = true;
                 x.pop();
                 y.pop();
                 //다음 연결되어있느면(1이면) 확인하고 최단거리 바꿔주기
+                //동 (y+1)
+                //동쪽에 길이있고, 방문한 적이 없으면->이건 한번 더 생각해보기 ->해야함
+                
+                //마지막 도착하면 끝내기
+                if(xt == c && yt == r){
+                    cout << sp[xt][yt];
+                    return 0;
+                }
+                    
+                
+                if(miro[xt][yt+1] == 1 && visit[xt][yt+1] == false){
+                    if(sp[xt][yt] + 1 < sp[xt][yt+1])
+                        sp[xt][yt+1] = sp[xt][yt] + 1;
+                    x.push(xt);
+                    y.push(yt+1);
+                    visit[xt][yt+1] = true;
+                }
+                //서 (y-1)
+                if(miro[xt][yt-1] == 1 && visit[xt][yt-1] == false){
+                    if(sp[xt][yt] + 1 < sp[xt][yt-1])
+                        sp[xt][yt-1] = sp[xt][yt] + 1;
+                    x.push(xt);
+                    y.push(yt-1);
+                    visit[xt][yt-1] = true;
+                }
+                //남 (x+1)
+                if(miro[xt+1][yt] == 1 && visit[xt+1][yt] == false){
+                    if(sp[xt][yt] + 1 < sp[xt+1][yt])
+                        sp[xt+1][yt] = sp[xt][yt] + 1;
+                    x.push(xt+1);
+                    y.push(yt);
+                    visit[xt+1][yt] = true;
+                }
+                //북 (x-1)
+                if(miro[xt-1][yt] == 1 && visit[xt-1][yt] == false){
+                    if(sp[xt][yt] + 1 < sp[xt-1][yt])
+                        sp[xt-1][yt] = sp[xt][yt] + 1;
+                    x.push(xt-1);
+                    y.push(yt);
+                    visit[xt-1][yt] = true;
+                }
                 //방문했으면 visit true
-                //
+                
+             
             }
         }
     }
